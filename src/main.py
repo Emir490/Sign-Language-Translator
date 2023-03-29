79d001eba2b6de11218cee8ae20541615893f377
Emir490 <chido_emir@hotmail.com>
2023-03-28 17:02:38 -0700

    Files created for make the dataset

--- src/main.py
+++ src/main.py
@@ -1,16 +1,19 @@
 import pickle
 import matplotlib.pyplot as plt
+from PIL import Image
 
 # Load the pickle file
 with open('data/raw/PALABRAS.pickle', 'rb') as f:
-    data = pickle.load(f)
+    data = pickle.load(f)   
+    
+print(data)
     
 # Extract the image and label data
-images = data[0]
-labels = data[1]
+# images = data[0]
+# labels = data[1]
 
 # Display the first 5 images with their labels
-for i in range(5):
-    plt.imshow(images[i])
-    plt.title(f'Label: {labels[i]}')
-    plt.show()
+# for i in range(5):
+#     print(images[i])
+#     plt.title(f'Label: {labels[i]}')
+#     plt.show()
