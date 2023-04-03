bf0aabd176406bd7f3baca6564d5dab7de99f0f3
Emir490 <chido_emir@hotmail.com>
2023-04-03 11:47:08 -0700

    Testing model prediction

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,10 @@
 import cv2
-import mediapipe as mp
+import numpy as np
+from keras.models import load_model, Sequential
 from src.data.make_dataset import mp_holistic, mp_drawing, mediapipe_detection, extract_keypoints, draw_landmarks
+from utils.index import actions
+
+model: Sequential = load_model('model')
 
 sequence = []
 sentence = []
@@ -19,6 +23,15 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
         
         # Draw landmarks
         draw_landmarks(image, results)
+        
+        # 2. Prediction Logic
+        keypoints = extract_keypoints(results)
+        sequence.append(keypoints)
+        sequence = sequence[-30:]
+        
+        if len(sequence) == 30:
+            res = model.predict(np.expand_dims(sequence, axis=0))[0]
+            print(actions[np.argmax(res)])
         
         # Show to screen
         cv2.imshow('OpenCV Feed', image)
