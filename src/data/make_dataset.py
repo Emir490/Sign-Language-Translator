7992c4c28f6690af15266b7cd0875372514da548
ValdezFOmar <omarantoniovaldezf2@gmail.com>
2023-04-05 14:00:20 -0700

    Basic UI

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -4,39 +4,30 @@ window = tkinter.Tk()
 window.geometry("400x400")
 # window.state("zoomed")
 
-list_box = tkinter.Listbox(window)
+# List Boxes
+listCategories = tkinter.Listbox(window)
+listSigns = tkinter.Listbox(window)
 
+# Elements of the listbox
 list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'LL','M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'RR', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 for i in range(len(list)):
-    list_box.insert(tkinter.END,list[i])
+    listCategories.insert(tkinter.END,list[i])
+    listSigns.insert(tkinter.END,list[i])
 
-list_box.pack()
-# label = tkinter.Label(window, text="Hola mundo", bg = "green")
-# # Coloca la eqtiqueta en la posicion indicada
-# # side = tkinter.LEFT
-# # label.pack(fill = tkinter.X)
+# Create scrollbars
+sbCategories = tkinter.Scrollbar(window, orient=tkinter.VERTICAL, command=listCategories.yview)
+listCategories.config(yscrollcommand=sbCategories.set)
 
-# def hello(nombre):
-#     print("Hello " + nombre)
+sbSigns = tkinter.Scrollbar(window, orient=tkinter.VERTICAL, command=listSigns.yview)
+listSigns.config(yscrollcommand=sbSigns.set)
 
-# #button = tkinter.Button(window,text="Clic",padx=50,pady=50,command=hello)
-# button1 = tkinter.Button(window,text="Hello",padx=50,pady=50,command=lambda: hello("Omar"))
-# # button1.pack()
+# Show ListBoxes
+listCategories.grid(row=1,column=0)
+sbCategories.grid(row=1,column=1,sticky=tkinter.NS)
 
-# # TextBox
-# textbox = tkinter.Entry(window,font="Helvetica 12")
-# # textbox.pack()
+listSigns.grid(row=1,column=2)
+sbSigns.grid(row=1,column=3,sticky=tkinter.NS)
 
-# def imprimir():
-#     print(textbox.get())
-
-# button2 = tkinter.Button(window,text='Get Text',command=imprimir)
-# # button2.pack()
-
-# button1.grid(row=0,column=0)
-# textbox.grid(row=1,column=1)
-# button2.grid(row=2,column=2)
-# label.grid(row=1,column=0)
-
+# Show window
 window.mainloop()
--- src/data/tkinter_test.py
+++ src/data/tkinter_test.py
@@ -14,3 +14,30 @@ my_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 my_listbox.config(yscrollcommand=my_scrollbar.set)
 
 root.mainloop()
+
+# label = tkinter.Label(window, text="Hola mundo", bg = "green")
+# # Coloca la eqtiqueta en la posicion indicada
+# # side = tkinter.LEFT
+# # label.pack(fill = tkinter.X)
+
+# def hello(nombre):
+#     print("Hello " + nombre)
+
+# #button = tkinter.Button(window,text="Clic",padx=50,pady=50,command=hello)
+# button1 = tkinter.Button(window,text="Hello",padx=50,pady=50,command=lambda: hello("Omar"))
+# # button1.pack()
+
+# # TextBox
+# textbox = tkinter.Entry(window,font="Helvetica 12")
+# # textbox.pack()
+
+# def imprimir():
+#     print(textbox.get())
+
+# button2 = tkinter.Button(window,text='Get Text',command=imprimir)
+# # button2.pack()
+
+# button1.grid(row=0,column=0)
+# textbox.grid(row=1,column=1)
+# button2.grid(row=2,column=2)
+# label.grid(row=1,column=0)
